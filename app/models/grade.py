2652ed8bc2f0b212843f204f65ffe2e371bf2701
SparkLeau <lbossy@guardiaschool.fr>
2025-02-07 16:42:36 +0100

    form add notes teacher + delete | probleme actualisation

--- app/models/grade.py
+++ app/models/grade.py
@@ -6,10 +6,9 @@ class GradeModel:
         self.db = Database()
 
     def add_grade(self, teacher_id, student_id, subject_id, grade, comment=""):
-        query = "INSERT INTO grades (teacher_id, student_id, subject_id, grade, comment) VALUES (%s, %s, %s, %s, %s)"
-        return self.db.execute(
-            query, (teacher_id, student_id, subject_id, grade, comment)
-        )
+        query = "INSERT INTO grades (student_id, teacher_id, subject_id, grade, comment) VALUES (%s, %s, %s, %s, %s)"
+        self.db.execute(query, (student_id, teacher_id, subject_id, grade, comment))
+        
 
     def get_student_grades(self, teacher_id, student_id):
         query = """
@@ -23,11 +22,11 @@ class GradeModel:
 
     def delete_grade(self, teacher_id, grade_id):
         query = "DELETE FROM grades WHERE teacher_id = %s AND id = %s"
-        return self.db.execute(query, (teacher_id, grade_id))
+        self.db.execute(query, (teacher_id, grade_id))
 
     def get_student_grade_by_student(self, student_id):
         query = """
-        SELECT g.grade, g.comment, g.created_at AS date_added, sub.name AS subject_name, t.first_name AS teacher_name
+        SELECT g.id, g.grade, g.comment, g.created_at AS date_added, sub.name AS subject_name, t.first_name AS teacher_name
         FROM grades g
         JOIN subjects sub ON g.subject_id = sub.id
         JOIN teachers t ON g.teacher_id = t.id
--- app/views/teacher_views.py
+++ app/views/teacher_views.py
@@ -10,6 +10,8 @@ from flask import (
 
 from app.controllers.student_controller import StudentController
 from app.controllers.teacher_controller import TeacherController
+from app.forms.forms_add_grade import AddGradeForm
+from app.forms.forms_delete_grade import DeleteGradeForm
 from app.decorators import require_teacher
 
 
@@ -37,66 +39,80 @@ class TeacherViews:
                 "teacher/dashboard_teacher.html", classes=classes
             )
 
-        @self.teacher_bp.route("/class/<int:class_id>")
+        @self.teacher_bp.route("/class/<int:class_id>", methods=["GET", "POST"])
         def class_students(class_id):
             teacher_id = session.get("user_id")
+            
             student_class = self.student_controller.get_student_class(class_id)
-            teacher_classes = self.teacher_controller.get_teacher_classes(
-                teacher_id
-            )
-            subjects = self.teacher_controller.get_subject_by_teacher(
-                session.get("user_id")
-            )
+
+            teacher_classes = self.teacher_controller.get_teacher_classes(teacher_id)
+            subjects = self.teacher_controller.get_subject_by_teacher(teacher_id)
+
+            form = AddGradeForm()
+            form.subject_id.choices = [(subject["id"], subject["name"]) for subject in subjects]
+
+            if form.validate_on_submit():
+                student_id = form.student_id.data
+                subject_id = form.subject_id.data
+                grade = form.grade.data
+                comment = form.comment.data
+
+                if not student_id or not student_id.isdigit():
+                    flash("L'ID de l'étudiant est invalide.", "error")
+                    return redirect(url_for("teacher_bp.class_students", class_id=class_id))
+
+                student_id = int(student_id) 
+                if not grade or not (0 <= float(grade) <= 20):
+                    flash("La note doit être un nombre entre 0 et 20.", "error")
+                    return redirect(url_for("teacher_bp.class_students", class_id=class_id))
+
+                self.teacher_controller.add_grade(teacher_id, student_id, subject_id, grade, comment)
+
+                flash("Note ajoutée avec succès !", "success")
+                return redirect(url_for("teacher_bp.class_students", class_id=class_id))
+
+
             return render_template(
                 "teacher/class_students.html",
                 students=student_class,
                 class_id=class_id,
                 classes=teacher_classes,
                 subjects=subjects,
+                form=form 
             )
 
-        @self.teacher_bp.route("/add_grade", methods=["POST"])
-        def add_grade():  # TODO: ajouter un coef pour chaque matière
-            teacher_id = session.get("user_id")
-            student_id = request.form["student_id"]
-            subject_id = request.form["subject_id"]
-            grade = request.form["grade"]
-            comment = request.form["comment"]
 
-            self.controller.add_grade(
-                teacher_id, student_id, subject_id, grade, comment
-            )
-
-            student_info = self.student_controller.get_student_info(student_id)
-            class_id = student_info["class_id"] if student_info else None
-
-            return redirect(
-                url_for("teacher_bp.class_students", class_id=class_id)
-            )
 
         @self.teacher_bp.route("/student/<int:student_id>")
         def student_grades(student_id):
-            teacher_id = session.get("user_id")
-            grades = self.teacher_controller.get_student_grades(
-                teacher_id, student_id
-            )
+            form = DeleteGradeForm()
+
+            grades = self.student_controller.get_student_grades(student_id)
             student_info = self.student_controller.get_student_info(student_id)
             class_id = student_info["class_id"]
 
+
             return render_template(
                 "teacher/student_grades.html",
+                form=form,
                 grades=grades,
                 class_id=class_id,
                 student_id=student_id,
             )
 
-        @self.teacher_bp.route("/delete_grade", methods=["POST"])
-        def delete_grade():
+        @self.teacher_bp.route("/delete_grade/<int:student_id>", methods=["POST"])
+        def delete_grade(student_id):
             teacher_id = session.get("user_id")
-            grade_id = request.form["grade_id"]
-            student_id = request.form.get("student_id")
-            self.teacher_controller.delete_grade(teacher_id, grade_id)
-            flash("Note supprimée avec succès !")
+
+            form = DeleteGradeForm()
+
+            if form.validate_on_submit():
+                grade_id = form.grade_id.data
+                self.teacher_controller.delete_grade(teacher_id, grade_id)
+                
+                flash("Note supprimée avec succès !")
+            else:
+                print("Erreur lors de la suppression de la note.", form.errors)
 
             return redirect(
                 url_for("teacher_bp.student_grades", student_id=student_id)
